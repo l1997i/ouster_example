d7e6d75e3ca0402d0da7564a657b4be6396fb234
Li (Luis) Li <luis.li@pitt.edu>
2021-06-15 11:05:02 +0100

    feat: run perfectly

--- ouster_ros/src/out_node.cpp
+++ ouster_ros/src/out_node.cpp
@@ -23,6 +23,7 @@ void pcdSubscribePointCloud(const sensor_msgs::PointCloud2ConstPtr& lidar_messag
     counter++;
     
     std::string file_name = pcd_path + std::to_string(counter) + ".pcd";
+    ROS_INFO_STREAM(file_name);
     pcl::io::savePCDFile(file_name, point_cloud);
     
 }
@@ -35,6 +36,7 @@ void binSubscribePointCloud(const sensor_msgs::PointCloud2ConstPtr& lidar_messag
 
     std::string file_name = bin_path + std::to_string(counter) + ".bin";
     std::ofstream binFile(file_name.c_str(), std::ios::out | std::ios::binary);
+    ROS_INFO_STREAM(file_name);
 
     for (unsigned int j = 0; j < cloud->size(); j++)
     {
@@ -47,27 +49,30 @@ void binSubscribePointCloud(const sensor_msgs::PointCloud2ConstPtr& lidar_messag
 }
 
 int main(int argc, char **argv) {
+    std::string lidar_topic = "/os_cloud_node/points";
     ros::init(argc, argv, "point_cloud_subscriber");
     ros::NodeHandle node_handle;
-    cloud_mode = node_handle.param("cloud_mode", std::string{});
-    out_path = node_handle.param("out_path", std::string{});
+    cloud_mode = node_handle.param("/out_node/cloud_mode", std::string{});
+    out_path = node_handle.param("/out_node/out_path", std::string{});
     mkdir(out_path.c_str(), 0777);
 
+    ros::Subscriber point_cloud_sub;
+
     if (cloud_mode == "pcd") {
+        ROS_INFO_STREAM("Now [PCD] Mode...");
         pcd_path = out_path + "/pcd/";
         mkdir(pcd_path.c_str(), 0777);
-        ros::Subscriber point_cloud_sub = 
-            node_handle.subscribe("/os_cloud_node/points", 1, pcdSubscribePointCloud);
+        point_cloud_sub = 
+            node_handle.subscribe(lidar_topic, 1, pcdSubscribePointCloud);
     }
     else if (cloud_mode == "bin") {
+        ROS_INFO_STREAM("Now [BIN] Mode...");
         bin_path = out_path + "/bin/";
         mkdir(bin_path.c_str(), 0777);
-        ros::Subscriber point_cloud_sub = 
-            node_handle.subscribe("/os_cloud_node/points", 1, binSubscribePointCloud);
+        point_cloud_sub = 
+            node_handle.subscribe(lidar_topic, 1, binSubscribePointCloud);
     }
 
-    
     ros::spin();
     return 0;
 }
-
